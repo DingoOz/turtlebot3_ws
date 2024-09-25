c94d89d07d4d85eb71c0d84529703ef4fcdb7a19
DingoOz <dingo.australia@gmail.com>
2024-09-25 21:31:21 +1000

    This breaks dashboard, it fixes many issues but now result in a seg fault to hunt down on updateFrame

--- src/dashboard_turtlebot3_cpp/include/dashboard_turtlebot3_cpp/dashboard_node.hpp
+++ src/dashboard_turtlebot3_cpp/include/dashboard_turtlebot3_cpp/dashboard_node.hpp
@@ -3,18 +3,36 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/image.hpp>
 #include <QMainWindow>
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QDockWidget>
 #include <QTimer>
 #include <QProgressBar>
+#include <QImage>
+#include <QPixmap>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+
+class VideoWidget : public QLabel {
+  Q_OBJECT
+
+public:
+  explicit VideoWidget(QWidget* parent = nullptr);
+  virtual ~VideoWidget() = default;
+  void updateFrame(const cv::Mat& frame);
+
+private:
+  QImage mat_to_qimage(const cv::Mat& mat);
+};
 
 class TwistWidget : public QWidget {
   Q_OBJECT
 
 public:
   explicit TwistWidget(QWidget* parent = nullptr);
+  virtual ~TwistWidget() = default;
   void updateTwist(const geometry_msgs::msg::Twist::SharedPtr msg);
 
 private:
@@ -29,11 +47,15 @@ class DashboardNode : public rclcpp::Node {
 public:
   DashboardNode();
   geometry_msgs::msg::Twist::SharedPtr getLatestTwist();
+  sensor_msgs::msg::Image::SharedPtr getLatestImage();
 
 private:
   void twistCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
+  void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
   geometry_msgs::msg::Twist::SharedPtr latest_twist_;
+  sensor_msgs::msg::Image::SharedPtr latest_image_;
 };
 
 class DashboardWindow : public QMainWindow {
@@ -41,6 +63,7 @@ class DashboardWindow : public QMainWindow {
 
 public:
   DashboardWindow(std::shared_ptr<DashboardNode> node, QWidget* parent = nullptr);
+  virtual ~DashboardWindow() = default;
 
 private slots:
   void updateWidgets();
@@ -48,6 +71,7 @@ private slots:
 private:
   std::shared_ptr<DashboardNode> dashboard_node_;
   TwistWidget* twist_widget;
+  VideoWidget* video_widget;
 };
 
 #endif // DASHBOARD_NODE_HPP
--- src/dashboard_turtlebot3_cpp/src/dashboard_node.cpp
+++ src/dashboard_turtlebot3_cpp/src/dashboard_node.cpp
@@ -1,6 +1,46 @@
-// dashboard_node.cpp
 #include "dashboard_turtlebot3_cpp/dashboard_node.hpp"
 #include <QApplication>
+#include <QDebug>
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QDockWidget>
+#include <QTimer>
+#include <QProgressBar>
+#include <QMetaObject>
+
+VideoWidget::VideoWidget(QWidget* parent) : QLabel(parent) {
+  setScaledContents(true);
+}
+
+void VideoWidget::updateFrame(const cv::Mat& frame) {
+  if (frame.empty()) {
+    qDebug() << "Received empty frame";
+    return;
+  }
+  
+  QImage qimg = mat_to_qimage(frame);
+  if (qimg.isNull()) {
+    qDebug() << "Failed to convert cv::Mat to QImage";
+  } else {
+    qDebug() << "Updating frame: " << qimg.width() << "x" << qimg.height();
+    QPixmap pixmap = QPixmap::fromImage(qimg);
+    if (pixmap.isNull()) {
+      qDebug() << "Failed to convert QImage to QPixmap";
+    } else {
+      setPixmap(pixmap);
+    }
+  }
+}
+
+QImage VideoWidget::mat_to_qimage(const cv::Mat& mat) {
+  if(mat.type() == CV_8UC3) {
+    cv::Mat rgb;
+    cv::cvtColor(mat, rgb, cv::COLOR_BGR2RGB);
+    return QImage((uchar*)rgb.data, rgb.cols, rgb.rows, rgb.step, QImage::Format_RGB888);
+  }
+  return QImage();
+}
 
 TwistWidget::TwistWidget(QWidget* parent) : QWidget(parent) {
   layout = new QVBoxLayout(this);
@@ -8,18 +48,17 @@ TwistWidget::TwistWidget(QWidget* parent) : QWidget(parent) {
   angular_label = new QLabel("Angular: x=0.00, y=0.00, z=0.00", this);
   
   linear_progress = new QProgressBar(this);
-  linear_progress->setRange(-100, 100);  // Assuming max speed is 1 m/s
+  linear_progress->setRange(-100, 100);
   linear_progress->setFormat("Linear X: %v");
   
-  // Add new progress bar for angular velocity
   angular_progress = new QProgressBar(this);
-  angular_progress->setRange(-100, 100);  // Assuming max angular velocity is 1.5 rad/s
+  angular_progress->setRange(-100, 100);
   angular_progress->setFormat("Angular Z: %v");
   
   layout->addWidget(linear_label);
   layout->addWidget(angular_label);
   layout->addWidget(linear_progress);
-  layout->addWidget(angular_progress);  // Add the new progress bar to the layout
+  layout->addWidget(angular_progress);
   setLayout(layout);
 }
 
@@ -33,46 +72,87 @@ void TwistWidget::updateTwist(const geometry_msgs::msg::Twist::SharedPtr msg) {
     .arg(msg->angular.y, 0, 'f', 2)
     .arg(msg->angular.z, 0, 'f', 2));
   
-  // Update linear velocity progress bar
-  int linear_progress_value = static_cast<int>(msg->linear.x * 100);  // Scale to -100 to 100
+  int linear_progress_value = static_cast<int>(msg->linear.x * 100);
   linear_progress->setValue(linear_progress_value);
   
-  // Update angular velocity progress bar
-  int angular_progress_value = static_cast<int>(msg->angular.z / 1.5 * 100);  // Scale to -100 to 100, assuming max is 1.5 rad/s
+  int angular_progress_value = static_cast<int>(msg->angular.z / 1.5 * 100);
   angular_progress->setValue(angular_progress_value);
 }
 
 DashboardNode::DashboardNode() : Node("dashboard_turtlebot3") {
   twist_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
     "cmd_vel", 10, std::bind(&DashboardNode::twistCallback, this, std::placeholders::_1));
+  image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+    "webcam/image_raw", 10, std::bind(&DashboardNode::imageCallback, this, std::placeholders::_1));
 }
 
 geometry_msgs::msg::Twist::SharedPtr DashboardNode::getLatestTwist() {
   return latest_twist_;
 }
 
+sensor_msgs::msg::Image::SharedPtr DashboardNode::getLatestImage() {
+  return latest_image_;
+}
+
 void DashboardNode::twistCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {
   latest_twist_ = msg;
+}
+
+void DashboardNode::imageCallback(const sensor_msgs::msg::Image::SharedPtr msg) {
+  RCLCPP_INFO(this->get_logger(), "Received image frame");
+  latest_image_ = msg;
 }
 
 DashboardWindow::DashboardWindow(std::shared_ptr<DashboardNode> node, QWidget* parent)
   : QMainWindow(parent), dashboard_node_(node) {
   setWindowTitle("Turtlebot3 Dashboard");
-  setGeometry(100, 100, 400, 350);  // Increased height to 350
+  setGeometry(100, 100, 800, 600);
+
+  video_widget = new VideoWidget(this);
+  video_widget->setMinimumSize(640, 480);
+  setCentralWidget(video_widget);
+
   twist_widget = new TwistWidget(this);
   QDockWidget* dock = new QDockWidget("Twist", this);
   dock->setWidget(twist_widget);
   dock->setFeatures(QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetFloatable);
-  addDockWidget(Qt::LeftDockWidgetArea, dock);
+  addDockWidget(Qt::BottomDockWidgetArea, dock);
+
   QTimer* timer = new QTimer(this);
   connect(timer, &QTimer::timeout, this, &DashboardWindow::updateWidgets);
-  timer->start(100);  // Update every 100ms
+  timer->start(33);  // Update at ~30 FPS
 }
 
 void DashboardWindow::updateWidgets() {
   auto twist_msg = dashboard_node_->getLatestTwist();
   if (twist_msg) {
     twist_widget->updateTwist(twist_msg);
+  }
+
+  auto image_msg = dashboard_node_->getLatestImage();
+  if (image_msg) {
+    try {
+      cv::Mat frame = cv_bridge::toCvShare(image_msg, "bgr8")->image;
+      if (frame.empty()) {
+        RCLCPP_WARN(dashboard_node_->get_logger(), "Received empty frame");
+      } else {
+        RCLCPP_INFO(dashboard_node_->get_logger(), "Processing frame: %dx%d", frame.cols, frame.rows);
+        
+        QMetaObject::invokeMethod(video_widget, [this, frame]() {
+          try {
+            video_widget->updateFrame(frame.clone());  // Use clone() to ensure deep copy
+          } catch (const std::exception& e) {
+            qDebug() << "Exception in updateFrame: " << e.what();
+          }
+        }, Qt::QueuedConnection);
+      }
+    } catch (cv_bridge::Exception& e) {
+      RCLCPP_ERROR(dashboard_node_->get_logger(), "CV bridge exception: %s", e.what());
+    } catch (std::exception& e) {
+      RCLCPP_ERROR(dashboard_node_->get_logger(), "Exception in updateWidgets: %s", e.what());
+    }
+  } else {
+    RCLCPP_WARN_THROTTLE(dashboard_node_->get_logger(), *dashboard_node_->get_clock(), 1000, "No image received");
   }
 }
 
